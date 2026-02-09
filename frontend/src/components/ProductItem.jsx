9d4e2738a52f5816db7727dc17a2636f067e418c
Victoriousm <mukadiamvictorious@gmail.com>
2026-02-09 19:55:20 +0200

    resized images

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -4,23 +4,25 @@ import { ShopContext } from '../context/ShopContext'
 import { Link } from 'react-router-dom'
 
 const ProductItem = ({id,image,name,price}) => {
-  const {currency} = useContext(ShopContext)
 
+    const {currency} = useContext(ShopContext)
+    
   return (
-    <Link 
-      className="text-gray-700 cursor-pointer block w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2" 
-      to={`/product/${id}`}
-    >
-      <div className="overflow-hidden rounded-lg">
-        <img 
-          className="hover:scale-105 transition ease-in-out 
-                     w-full h-auto object-cover" 
-          src={image[0]} 
-          alt={name} 
-        />
+    <Link className='text-grey-700 cursor-pointer' to={`/product/${id}`}>
+      {React.createElement('span', null,)}
+      <div className='overflow-hidden'>
+        <img
+  className="
+    hover:scale-110 transition ease-in-out
+    w-full object-cover rounded-full
+    h-40 sm:h-60 md:h-72 lg:h-80
+  "
+  src={image[0]}
+  alt={name}
+/>
       </div>
-      <p className="pt-2 text-sm font-medium truncate">{name}</p>
-      <p className="text-sm font-semibold">{currency}{price}</p>
+      <p className='pt-3 pb-1 text-sm'>{name}</p>
+      <p className='text-sm font-medium'>{currency}{price}</p>
     </Link>
   )
 }
