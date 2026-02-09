765711afd8d234b681439bdda584d3e02298ad87
Victoriousm <mukadiamvictorious@gmail.com>
2026-02-09 19:07:50 +0200

    resized images

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -1,20 +1,28 @@
 /* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
 import React, { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { Link } from 'react-router-dom'
 
 const ProductItem = ({id,image,name,price}) => {
-
-    const {currency} = useContext(ShopContext)
-    
+  const {currency} = useContext(ShopContext)
+  
   return (
-    <Link className='text-grey-700 cursor-pointer' to={`/product/${id}`}>
-      {React.createElement('span', null,)}
-      <div className='overflow-hidden'>
-        <img className='hover:scale-110 transition ease-in-out h-80 w-full object-cover rounded-full' src={image[0]} alt="" />
+    <Link 
+      className="text-gray-700 cursor-pointer block max-w-xs mx-auto sm:max-w-sm md:max-w-md" 
+      to={`/product/${id}`}
+    >
+      <div className="overflow-hidden flex justify-center">
+        <img 
+          className="hover:scale-110 transition ease-in-out 
+                     h-40 w-40 object-cover rounded-full 
+                     sm:h-60 sm:w-60 md:h-80 md:w-full" 
+          src={image[0]} 
+          alt={name} 
+        />
       </div>
-      <p className='pt-3 pb-1 text-sm'>{name}</p>
-      <p className='text-sm font-medium'>{currency}{price}</p>
+      <p className="pt-3 pb-1 text-sm text-center truncate">{name}</p>
+      <p className="text-sm font-medium text-center">{currency}{price}</p>
     </Link>
   )
 }
